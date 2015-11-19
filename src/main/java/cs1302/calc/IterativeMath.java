3e7b576ce43974838813d45428cbf43317e2e468
Nicholas <ndesai94@uga.edu>
2015-11-18 20:27:07 -0500

    trial 1

--- src/main/java/cs1302/calc/IterativeMath.java
+++ src/main/java/cs1302/calc/IterativeMath.java
@@ -13,42 +13,108 @@ public class IterativeMath implements Math {
 
     public int inc(int n) {
         throw new UnsupportedOperationException();
+	return n+1;
     } // inc
 
     public int dec(int n) {
         throw new UnsupportedOperationException();
+	return n-1;
     } // dec
 
     public int add(int lhs, int rhs) {
         throw new UnsupportedOperationException();
+	int sum = lhs;
+	while (rhs>0) {
+	    sum=inc(sum);
+	    rhs=dec(rhs);
+	}
+	return sum;
     } // add
 
     public int sub(int lhs, int rhs) {
         throw new UnsupportedOperationException();
+	int sum = 0;
+	if (rhs>lhs) {
+	    return 0;
+	}
+	while (lhs>rhs) {
+	    sum = inc(sum);
+	    lhs = dec(lhs);
+	}
+	return sum;
     } // sub
 
     public int mul(int lhs, int rhs) {
         throw new UnsupportedOperationException();
+	int product = 0;
+	if (rhs==1) {
+	    return lhs;
+	}
+	if (rhs==0) {
+	    return 0;
+	}
+	while (rhs>0) {
+	    product = add(product,lhs);
+	    rhs = dec(rhs);
+	}
+	return product;
     } // mul
 
     public int div(int lhs, int rhs) {
         throw new UnsupportedOperationException();
+	int quotient = 0;
+	if (rhs==1) {
+	    return lhs;
+	}
+	if (rhs==0) {
+	    return 0;
+	}
+	if (rhs>lhs) {
+	    return 0;
+	}
+	while (lhs>0) {
+	    lhs = sub(lhs,rhs);
+	    quotient = inc(quotient);
+	}
+	return quotient;
     } // div
 
     public int fac(int n) {
         throw new UnsupportedOperationException();
+	int factorial = 1;
+	while (n>0) {
+	    factorial = mul(factorial,n);
+	    n = dec(n);
+	}
+	return factorial;
     } // fac
     
     public int pow(int lhs, int rhs) {
         throw new UnsupportedOperationException();
+	int power = 1;
+	while (rhs>0) {
+	    power = mul(power,lhs);
+	    rhs = dec(rhs);
+	}
+	return power;
     } // pow
     
     public int lshift(int lhs, int rhs) {
         throw new UnsupportedOperationException();
+	while (rhs>0) {
+	    lhs = mul(lhs,2);
+	    rhs = dec(rhs);
+	}
+	return lhs;
     } // lshift
     
     public int rshift(int lhs, int rhs) {
         throw new UnsupportedOperationException();
+	while (rhs>0) {
+	    lhs = div(lhs,2);
+	    rhs = dec(rhs);
+	}
+	return lhs;
     } // rshift
 
 } // IterativeMath
--- src/main/java/cs1302/calc/RecursiveMath.java
+++ src/main/java/cs1302/calc/RecursiveMath.java
@@ -13,34 +13,69 @@ public class RecursiveMath implements Math {
 
     public int inc(int n) {
         throw new UnsupportedOperationException();
+	return n+1;
     } // inc
 
     public int dec(int n) {
         throw new UnsupportedOperationException();
+	return n-1;
     } // dec
 
     public int add(int lhs, int rhs) {
         throw new UnsupportedOperationException();
+	if (rhs==0) {
+	    return lhs;
+	}
+	return add(inc(lhs),dec(rhs));
     } // add
 
     public int sub(int lhs, int rhs) {
         throw new UnsupportedOperationException();
+	if (rhs==0) {
+	    return lhs;
+	}
+	if (rhs>lhs) {
+	    return 0;
+	}
+	return sub(dec(lhs),dec(rhs));
     } // sub
 
     public int mul(int lhs, int rhs) {
         throw new UnsupportedOperationException();
+	if (rhs==0) {
+	    return 0;
+	}
+	if (rhs==1) {
+	    return lhs;
+	}
+	return add(lhs,mul(lhs,pred(rhs)));
     } // mul
 
     public int div(int lhs, int rhs) {
         throw new UnsupportedOperationException();
+	if (rhs>lhs) {
+	    return 0;
+	}
+	if (lhs==rhs) {
+	    return 1;
+	}
+	return inc(div(sub(lhs,rhs),rhs));
     } // div
 
     public int fac(int n) {
         throw new UnsupportedOperationException();
+	if (n==0) {
+	    return 1;
+	}
+	return n * fac(dec(n));
     } // fac
     
     public int pow(int lhs, int rhs) {
         throw new UnsupportedOperationException();
+	if (rhs==0) {
+	    return 1;
+	}
+	return lhs * pow(lhs,dec(rhs));
     } // pow
     
     public int lshift(int lhs, int rhs) {
